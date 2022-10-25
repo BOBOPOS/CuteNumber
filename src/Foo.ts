b1b5854420e29e34254302279f0f50ca98fde40b
Bo Zhao <BZhao7@slb.com>
2022-10-25 10:47:49 +0800

    feat: add cutenumber function

--- src/Foo.ts
+++ src/Foo.ts
@@ -1,5 +1,20 @@
+
 export class Foo {
   bar() {
     return 'bar'
   }
 }
+
+export function isCuteNumber(numbers:number[]):boolean {
+  var evenNumbers = findEvenNumbers(numbers)
+  var maxNumber = findMaxNumber(evenNumbers)
+  return maxNumber>10;
+}
+
+function findEvenNumbers(numbers:number[]):number[]{
+  return numbers.filter(num => num%2 === 0);
+}
+function findMaxNumber(numbers:number[]):number {
+  return Math.max(...numbers)
+}
+
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,19 @@
 import {Foo} from '../src/Foo'
-
+import {isCuteNumber} from '../src/Foo'
 describe('Foo', () => {
   it('bar', () => {
     expect(new Foo().bar()).toEqual('bar')
   })
 })
+
+describe('find cute number', () => {
+  it('Provide numbers 1,3,5,7,8,12 and return true', () => {
+    const numbers = [1,3,5,7,8,12];
+
+    const actual:boolean = isCuteNumber(numbers)
+
+    expect(actual).toBeTruthy();
+
+  });
+  
+});
